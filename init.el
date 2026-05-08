b1f047e2d3e49de87d4bd69f23047e4717215ede
Gabriel Nogueira <gabrielscnogueira@gmail.com>
2026-05-08 14:59:55 -0300

    reorganiza repo: adiciona .gitignore e remove arquivos gerados rastreados por engano
    - Adiciona .gitignore cobrindo elpa/, eln-cache/, .cache/, custom.el,
      backups (*~, #*#), e arquivos de estado de sessão do Emacs
    - Remove do rastreamento git os backups automáticos templates/*.org~
    - Remove init.el.old (histórico preservado no git)

--- init.el
+++ init.el
@@ -1,110 +1,141 @@
-;; =================================
-;; Author: Gabriel da S. C. Nogueira
-;; GitHub: gscnogueira
-;; Emacs Version: 29.3
-;; =================================
-
-(eval-when-compile (require 'use-package))
-
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(eval-when-compile (require 'use-package))
 
 (use-package emacs
   :init
   (add-to-list 'default-frame-alist '(font . "JetBrains Mono-15"))
   :hook
   (prog-mode . display-line-numbers-mode)
+  (prog-mode . hl-line-mode)
+
 
   :custom
   (inhibit-splash-screen t)
   (tool-bar-mode nil)
   (menu-bar-mode nil)
   (scroll-bar-mode nil)
-  (fringe-mode 0)
   (global-visual-line-mode t)
   (column-number-mode t)
-  (global-hl-line-mode t)
-  (custom-file (expand-file-name "custom.el" user-emacs-directory))
   (apropos-sort-by-scores t)
-
-  :config
-  ;; Buffer-local variable (needs setq-default)
-  (setq-default left-margin-width 1) 
-  ;; Set left-margin-width to 0 on prog-mode
-  (add-hook 'prog-mode-hook (function(lambda () (setq left-margin-width 0))))
-  (load custom-file)
-
-  :bind
-  ("<mouse-8>" . switch-to-prev-buffer)
-  ("<mouse-9>" . switch-to-next-buffer)
+  (custom-file
+  (expand-file-name "custom.el" user-emacs-directory)
   )
+
+:config
+(setq-default left-margin-width 1) 
+;; Set left-margin-width to 0 on prog-mode
+(add-hook 'prog-mode-hook
+	  (function(lambda () (setq left-margin-width 0)))
+	  )
+(load custom-file)
+(setq make-backup-files nil)
+(setq frame-resize-pixelwise t)
+(setq window-resize-pixelwise t)
+(setq initial-major-mode 'org-mode) ;; org mode for initial buffer
+(setq native-comp-async-report-warnings-errors 'silent)
+(winner-mode 1)
+
+)
 
 (use-package modus-themes
   :ensure t
   :init
   (require-theme 'modus-themes)
   :config
-  (load-theme 'modus-vivendi-tinted)
+  (load-theme 'modus-vivendi)
   :custom
   (modus-themes-italic-constructs t)
   (modus-themes-bold-constructs t)
-  (modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
+  (modus-themes-to-toggle
+   '(modus-operandi-tinted modus-vivendi)
+   )
   )
 
-(use-package winner
-  :config (winner-mode t))
+(use-package dired
+  :custom
+  (dired-create-destination-dirs t)
+  :hook
+  (dired-mode . dired-hide-details-mode)
+  )
 
 (use-package org
-  :init
-  (setq org-capture-templates '(
-				("f" "FioCruz")
-				("ff" "Tarefa" entry
-				 (file+headline "fiocruz.org" "Tasks")
-				 (file "~/.emacs.d/templates/capture-todo.org"))
-
-				("p" "Pessoal")
-				("pp" "Tarefa" entry
-				 (file+headline "~/org/pessoal.org" "Tasks" )
-				 (file "~/.emacs.d/templates/capture-todo.org"))
-				("pe" "Evento" entry
-				 (file+headline "pessoal.org" "Eventos" )
-				 (file "~/.emacs.d/templates/evento.org"))
-				
-				("d" "Doutorado")
-				("dd" "Tarefa" entry
-				 (file+headline "doutorado.org" "Tasks")
-				 (file "~/.emacs.d/templates/capture-todo.org"))
-				("dr" "Reunião" entry
-				 (file+headline "doutorado.org" "Reuniões")
-				 (file "~/.emacs.d/templates/evento.org"))
-
-				("r" "Reading List" entry
-				 (file "reading_list.org")
-				 (file "~/.emacs.d/templates/reading_list.org"))
-				))
-  :hook
-  ((org-mode . org-indent-mode)
-   (org-babel-after-execute . org-redisplay-inline-images))
+  :hook (
+         (org-mode . org-indent-mode)
+         (org-agenda-mode . hl-line-mode)
+         (org-babel-after-execute . org-redisplay-inline-images)
+         )
   :bind
   (("C-c a" . org-agenda)
    ("C-c c" . org-capture))
   :custom
   (org-hide-emphasis-markers  t)
-  (org-startup-with-inline-images t)
-  (org-confirm-babel-evaluate nil)
-  (org-log-into-drawer t)
-  :config
-  (org-babel-do-load-languages 'org-babel-load-languages
-			       '((gnuplot . t) (shell . t)))
 
-  
+  (org-startup-with-inline-images t)
+
+  (org-confirm-babel-evaluate nil)
+
+  (org-todo-keywords
+   '((sequence "TODO(t)" "BLOCKED(b@)" "|" "DONE(d)" "CANCELLED(c@)"))
+   )
+
+  (org-agenda-span 'day)
+  (org-enforce-todo-dependencies t)
+  (org-enforce-todo-checkbox-dependencies t)
+  (org-hide-drawer-startup t)
+
+  :config
+  (defun my/org-prettify-checkboxes ()
+    (push '("[ ]" . "☐") prettify-symbols-alist)
+    (push '("[X]" . "☑") prettify-symbols-alist)
+    (push '("[-]" . "⊟") prettify-symbols-alist)
+    (prettify-symbols-mode 1))
+
+  (set-face-attribute 'variable-pitch nil :family "Literata" :height 160)
+  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-date nil :inherit 'fixed-pitch)
+  (add-hook 'org-mode-hook 'variable-pitch-mode)
+  (add-hook 'org-mode-hook #'my/org-prettify-checkboxes)
+  (setq org-agenda-files '("~/org/agenda/"))
   )
 
 
-(use-package gnuplot
-  :ensure t
-  :defer t)
+(setq org-agenda-tag-filter-preset '("-noagenda"))
+
+(setq org-capture-templates
+      '(
+        ("i" "Ideia" entry
+         (file+headline "~/org/agenda/doutorado.org" "Ideias")
+         "* %^{Título}\n:PROPERTIES:\n:CREATED: %U\n:END:\n%?"
+         )
+        ("l" "Tópicos para abordar na próxima reunião com o prof. Luís"
+         checkitem
+         (file+regexp "~/org/agenda/doutorado.org" "Reunião Luís")
+         "[ ] %?")
+        ("d" "Doutorado" entry
+         (file+headline "~/org/agenda/doutorado.org" "Tarefas")
+         "* TODO %^{Tarefa:}\n:PROPERTIES:\n:CREATED: %U\n:END:\n%?")
+        ("f" "Tarefa fiocruz" entry
+         (file+headline "~/org/agenda/fiocruz.org" "Tarefas")
+         "* TODO %^{Tarefa:}\n:PROPERTIES:\n:CREATED: %U\n:END:\n%?")
+	("D" "Disciplinas" entry
+	 (file+headline "~/org/agenda/disciplinas.org" "Tarefas")
+	 "* TODO %^{Tarefa:}\n:PROPERTIES:\n:CREATED: %U\n:END:\n%?")
+        ("F" "Tóipico de reunião (fiocruz)" entry
+         (file+headline "~/org/agenda/fiocruz.org" "Reuniões")
+         "* TODO %^{Tópico a ser discutido:}\n:PROPERTIES:\n:CREATED: %U\n:END:\n%?")
+	)
+      )
+      
 
 (use-package which-key
   :ensure t
@@ -131,7 +162,8 @@
 
 (use-package vertico
   :ensure t
-  :init (vertico-mode t)
+  :init 
+  (vertico-mode t)
   :bind
   (:map vertico-map
 	;; Restores TAB default behaviour
@@ -140,69 +172,47 @@
 	)
   )
 
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic)))
+
 (use-package consult
   :ensure t
   :bind
-  ("C-x b" . consult-buffer)
-  ("C-c C-x C-o" . consult-outline)
-  ("C-c m" . consult-line-multi)
-  ("C-c o" . consult-org-agenda))
+  (
+   ("C-x b" . consult-buffer)
+   ("C-c C-x C-o" . consult-outline)
+   ("C-c m" . consult-line-multi)
+   ("C-c o" . consult-org-agenda)
+   ("C-x j" . consult-imenu)
+   )
+  )
 
 (use-package corfu
   :ensure t
   :custom
   (global-corfu-mode t))
 
-(use-package orderless
-  :ensure t
-  :custom
-  (completion-styles '(orderless basic)))
-
-(use-package rainbow-delimiters
-  :ensure t
-  :hook
-  (prog-mode . rainbow-delimiters-mode))
-
 (use-package magit
-  :defer t
   :ensure t)
 
-(use-package haskell-mode
+(use-package diff-hl
   :ensure t
-  :defer t) 
+  :config
+  (global-diff-hl-mode t)
+  )
 
 (use-package ledger-mode
   :defer t
   :ensure t)
 
-(use-package pdf-tools
-  :ensure t
-  :defer t)
-
-(use-package lsp-mode
-  :ensure t
-  :commands lsp)
-
-(use-package pyvenv
-  :defer t
-  :ensure t)
-
-(use-package lsp-pyright
-  :ensure t
-  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
-  :hook
-  (python-mode . lsp)
-  )
-
-(use-package direnv
-  :ensure t)
-
 (use-package marginalia
   :ensure t
   :bind (:map minibuffer-local-map
-	      ("M-A" . marginalia-cycle))
-  :init
-  (marginalia-mode)
+              ("M-A" . marginalia-cycle))
+  :init 
+  (marginalia-mode 1)
   )
 
 (use-package embark
@@ -215,25 +225,104 @@
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
-(use-package org-roam
-  :ensure t
-  :bind
-  (("C-c i" . org-roam-node-insert)
-   ("C-c f" . org-roam-node-find)
-   ("C-c d" . org-roam-dailies-goto-date)
-   :map org-mode-map
-   ("C-c n" . org-roam-dailies-goto-next-note)
-   ("C-c p" . org-roam-dailies-goto-previous-note)
-   ("C-c r b" . org-roam-buffer-toggle))
-  :config
-  (org-roam-db-autosync-mode)
-  :custom
-  (org-roam-directory "~/SlipBox")
-  (org-roam-node-display-template
-   (concat "${title:*} " (propertize "${tags:*}" 'face 'org-tag))))
 
 (use-package vterm
+  :ensure t
+  :config
+  (add-hook 'vterm-mode-hook  'turn-off-evil-mode)
+  )
+
+(use-package lsp-mode
+  :init
+  :ensure t
+  :commands lsp
+  :config
+  (setq gc-cons-threshold 1000000000) ;; 1GB
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq lsp-signature-render-documentation nil)
+
+  ;; Configurações específicas para o lsp-ruff
+  (setq lsp-ruff-lint-select ["E","F", "I", "B"])
+
+  ;; Configurações flymake
+  (setq flymake-show-diagnostics-at-end-of-line nil)
+  (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  )
+
+(use-package copilot
+  :ensure t
+  :hook (prog-mode . copilot-mode)
+  :custom
+  (copilot-server-executable "/home/gabriel-nogueira/.npm-global/bin/copilot-language-server")
+  (copilot-idle-delay nil)
+  :bind (("C-c <tab>" . copilot-complete)
+         :map copilot-completion-map
+         ("C-c <return>" . copilot-accept-completion)
+         ("C-<tab>" . copilot-accept-completion-by-word)))
+
+(use-package gptel
+  :ensure t
+  :config
+  (setq gptel-model 'gpt-5.3-codex
+        gptel-backend (gptel-make-gh-copilot "Copilot"))
+  :bind
+  ("C-c g r" . gptel-rewrite)
+  ("C-c g a" . gptel-add)
+  )
+
+(use-package org-superstar
+  :ensure t
+  :hook (org-mode . org-superstar-mode))
+
+(use-package doom-modeline
+  :ensure t
+  :config
+  (doom-modeline-mode t)
+  )
+
+(use-package reformatter
+  :ensure t
+  :config
+  (reformatter-define ruff-format
+                      :program "ruff"
+                      :args '("format" "--line-length" "88" "-"))
+  (add-hook 'python-mode-hook #'ruff-format-on-save-mode))
+
+(use-package haskell-mode
+  :ensure t
+  :defer t)
+
+(use-package pyvenv
+  :defer t
   :ensure t)
 
+(use-package lsp-pyright
+  :ensure t
+  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :hook
+  (python-mode . lsp)
+  )
 
+(use-package docker-compose-mode
+  :ensure t)
 
+(use-package denote
+  :ensure t
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (
+   ("C-c n n" . denote-open-or-create )
+   )
+  :config
+  (setq denote-directory (expand-file-name "~/notes"))
+  )
+
+(use-package dockerfile-mode
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook
+  (prog-mode . rainbow-delimiters-mode)
+  )
